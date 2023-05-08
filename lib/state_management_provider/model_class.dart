dfee416fee275360e834e2b6b22b38e815e65e6b
RakibHossainMorul <67518360+RakibHossainMorul@users.noreply.github.com>
2023-05-08 15:56:37 +0600

    state_management_approach_added

--- lib/state_management_provider/model_class.dart
+++ lib/state_management_provider/model_class.dart
@@ -1,28 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:provider/provider.dart';
 
-/*
-//not using this class
-class CounterModel extends ChangeNotifier {
-  int _counterValue = 0;
-
-  int get counterValue => _counterValue;
-
-  void incrementCounter() {
-    _counterValue++;
-    notifyListeners();
-  }
-}
-*/
-
-class NamesModel extends ChangeNotifier {
+class MyWidgetState with ChangeNotifier {
+  final RegExp regExp = RegExp(r'[.,/@]');
+  final TextEditingController _controller = TextEditingController();
+  int counter = 0;
   List<String> _names = [];
 
-  List<String> get names => _names;
-
-  void addName(String name) async {
+  //adding controller data to list
+  void addName(String name) {
     _names.add(name);
     notifyListeners();
+  }
+
+  //Increment counter value and set data in shared prefercence
+  void incrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    counter++;
+    prefs.setInt('counter', counter);
+    notifyListeners();
+  }
+
+  //Retrieving counter value from shared prefercence
+  void loadCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    counter = (prefs.getInt('counter') ?? 0);
+    notifyListeners();
+  }
+
+  //remove counter value data from shared prefercence
+  void removeCounterValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('counter');
+    counter = (prefs.getInt('counter') ?? 0);
+    notifyListeners();
+  }
+
+  //Adding list data to local storage by shared preference
+  void addListData() async {
+    SharedPreferences prefs1 = await SharedPreferences.getInstance();
+    prefs1.setStringList("items", _names);
+    notifyListeners();
+  }
+
+  //Retrive data from local storage by shared preference
+  void loadListData() async {
+    SharedPreferences prefs2 = await SharedPreferences.getInstance();
+    List<String>? items = prefs2.getStringList("items");
+    if (items != null) {
+      _names = items;
+    }
+    notifyListeners();
+  }
+
+  //remove List value data from shared prefercence
+  void removeListValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('items');
+    _names = (prefs.getStringList('items') ?? []);
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
--- lib/state_management_provider/test.dart
+++ lib/state_management_provider/test.dart
@@ -1,74 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/state_management_provider/model_class.dart';
-import 'package:helloworld/state_management_provider/test2.dart';
 import 'package:provider/provider.dart';
-
-class CounterWidget extends StatefulWidget {
-  const CounterWidget({super.key});
-
-  @override
-  _CounterWidgetState createState() => _CounterWidgetState();
-}
-
-class _CounterWidgetState extends State<CounterWidget> {
-  int _localCounterValue = 0;
-
-  void _incrementLocalCounter() {
-    setState(() {
-      _localCounterValue++;
-    });
-  }
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final counterModel = Provider.of<CounterModel>(context);
-
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'App state counter value:',
-              ),
-              Text(
-                '${counterModel.counterValue}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              ElevatedButton(
-                onPressed: counterModel.incrementCounter,
-                child: const Text('Increment app state counter'),
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'Local state counter value:',
-              ),
-              Text(
-                '$_localCounterValue',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              ElevatedButton(
-                onPressed: _incrementLocalCounter,
-                child: const Text('Increment local state counter'),
-              ),
-              const Divider(
-                height: 10,
-                thickness: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const NamesListScreen(),
-                      ),
-                    );
-                  },
-                  child: const Text("Navigate page")),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+        body: ChangeNotifierProvider(
+          create: (context) => MyWidgetState(),
+          child: Consumer<MyWidgetState>(
+            builder: (context, myWidgetState, child) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            const Text(
+                              "Counter Value",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              myWidgetState.counter.toString(),
+                              style: const TextStyle(
+                                  fontSize: 100, color: Colors.red),
+                            ),
+                            ElevatedButton(
+                              onPressed: myWidgetState.incrementCounter,
+                              child: const Text("Increment Counter"),
+                            ),
+                            ElevatedButton(
+                              onPressed: myWidgetState.removeCounterValue,
+                              child: const Text("Remove counter value"),
+                            ),
+                            TextField(
+                              controller: myWidgetState._controller,
+                              decoration: const InputDecoration(
+                                hintText: "Enter Name",
+                                labelText: "Name",
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                if (myWidgetState._controller.text.isEmpty ||
+                                    myWidgetState.regExp.hasMatch(
+                                        myWidgetState._controller.text)) {
